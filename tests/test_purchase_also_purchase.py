a59a3b1548fae017d91120820f8539050ee219b7
Rohit Nawale <Rohit.Nawale@kibocommerce.com>
2023-05-16 19:02:16 +0530

    Add tests for purchase_also_purchase filters

--- tests/test_purchase_also_purchase.py
+++ tests/test_purchase_also_purchase.py
@@ -374,3 +374,119 @@ class PurchaseAlsoPurchaseTestCase(RecsTestCaseWithData):
         self._run_collab_recs_test('purchase_also_purchase', 2, recsets,
                                    expected_results, market=self.market, purchase_data_source="online_offline")
 
+class PurchaseAlsoPurchaseFiltersTestCase(RecsTestCaseWithData):
+    @classmethod
+    @patch_invalidations
+    def setUpClass(cls):
+        super(PurchaseAlsoPurchaseFiltersTestCase, cls).setUpClass()
+        # initializing purchase_also_purchase recsets
+        # filters with 30 day lookback
+        recs1 = {'filter_json': json.dumps({"type":"and","filters":[{"left":{"type":"field","field":"color"},
+        "type":"in","right":{"type":"function","value":"items_from_base_recommendation_on"}}]}),
+        'lookback': 30, 'global_recset': False, 'market': False, 'retailer_market_scope': False,
+                 'purchase_data_source': 'online'}
+
+        # filters with 7 day lookback
+        recs2 = {'filter_json': json.dumps({"type":"and","filters":[{"left":{"type":"field","field":"brand"},"type":"not in",
+        "right":{"type":"function","value":"items_from_base_recommendation_on"}},{"left":{"type":"field","field":"brand"},
+        "type":"not in","right":{"type":"function","value":"items_from_base_recommendation_on"}}]}),
+        'lookback': 7, 'global_recset': False, 'market': False, 'retailer_market_scope': False,
+                 'purchase_data_source': 'online'}
+
+        recsets_to_create = [recs1, recs2]
+
+        with invalidation_context():
+            for recset in recsets_to_create:
+                rec = recs_models.RecommendationSet.objects.create(
+                    algorithm='purchase_also_purchase',
+                    account=None if recset['global_recset'] else cls.account,
+                    lookback_days=recset['lookback'],
+                    filter_json=recset['filter_json'],
+                    retailer=cls.account.retailer,
+                    base_recommendation_on="none",
+                    geo_target="none",
+                    name="test",
+                    order="algorithm",
+                    version=1,
+                    product_catalog=dio_models.Schema.objects.get(id=cls.product_catalog_id),
+                    retailer_market_scope=cls._setup_retailer_market(recset['retailer_market_scope'], recset['market']),
+                    market=cls._setup_market(recset['market']),
+                    purchase_data_source=recset['purchase_data_source']
+                )
+                # for global recset which is not market we need to create a row in recommendation_set_dataset table
+                if rec.is_retailer_tenanted and not rec.is_market_or_retailer_driven_ds:
+                    recset_dataset = dio_models.Schema.objects.create(retailer=cls.account.retailer,
+                                                                      name='purchase_also_purchase')
+                    recs_models.RecommendationSetDataset.objects.create(
+                        recommendation_set_id=rec.id,
+                        dataset_id=recset_dataset.id,
+                        account_id=cls.account.id
+                    )
+                # add to queue if not already in queue
+                recs_models.PrecomputeQueue.objects.get_or_create(
+                    account=cls.set_account(rec, cls.account) if rec.is_retailer_tenanted
+                    else cls.set_account(rec),
+                    market=rec.market,
+                    retailer=rec.retailer if rec.retailer_market_scope else None,
+                    algorithm=rec.algorithm,
+                    lookback_days=rec.lookback_days,
+                    purchase_data_source=rec.purchase_data_source
+                )
+
+    def test_30_day_purchase_also_purchase_account_level_with_default_attribute_added_to_collab_sku_ranks_query(self):
+        recsets = recs_models.RecommendationSet.objects.filter(
+            Q(algorithm='purchase_also_purchase',
+              account=self.account,
+              lookback_days=30,
+              market=None,
+              retailer_market_scope=None,
+              purchase_data_source="online") |
+            Q(algorithm='purchase_also_purchase',
+              account=None,
+              lookback_days=30,
+              market=None,
+              retailer_market_scope=None,
+              purchase_data_source="online")
+        )
+
+        recs1_expected_result = [
+            ('TP-00001', [('SKU-00002', 1)]),
+            ('TP-00002', [('SKU-00001', 1)]),
+            ('TP-00003', [('SKU-00004', 1)]),
+            ('TP-00004', [('SKU-00003', 1)]),
+        ]
+
+        expected_results_arr = [recs1_expected_result]
+        expected_results = {}
+        for index, r in enumerate(recsets):
+            expected_results[r.id] = expected_results_arr[index]
+        self._run_collab_recs_test('purchase_also_purchase', 30, recsets,
+                                   expected_results, account=self.account) 
+
+    def test_7_day_purchase_also_purchase_account_level_with_multiple_filters_on_same_attribute(self):
+        recsets = recs_models.RecommendationSet.objects.filter(
+            Q(algorithm='purchase_also_purchase',
+              account=self.account,
+              lookback_days=7,
+              market=None,
+              retailer_market_scope=None,
+              purchase_data_source="online") |
+            Q(algorithm='purchase_also_purchase',
+              account=None,
+              lookback_days=7,
+              market=None,
+              retailer_market_scope=None,
+              purchase_data_source="online")
+        )
+
+        expected_results = {}
+        for r in recsets:
+            expected_results[r.id] = [
+                ('TP-00001', [('SKU-00004', 1), ('SKU-00002', 2), ('SKU-00006', 3), ('SKU-00005', 4), ('SKU-00003', 5)]),
+                ('TP-00004', [('SKU-00002', 1), ('SKU-00001', 2), ('SKU-00006', 3), ('SKU-00005', 4), ('SKU-00003', 5)]),
+                ('TP-00003', [('SKU-00002', 1), ('SKU-00004', 2), ('SKU-00001', 3)]),
+                ('TP-00005', [('SKU-00004', 1), ('SKU-00002', 2), ('SKU-00001', 3)]),
+                ('TP-00002', [('SKU-00004', 1), ('SKU-00003', 2), ('SKU-00001', 3), ('SKU-00006', 4), ('SKU-00005', 5)])
+            ]
+
+        self._run_collab_recs_test('purchase_also_purchase', 7, recsets, expected_results, account=self.account)
